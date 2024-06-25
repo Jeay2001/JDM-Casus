59058ceb13039589f6dafb0bcaa2bdf606a23373
Jeay <jeay2001@gmail.com>
2024-06-25 12:15:55 +0200

    V1

--- WellNexus/obj/Debug/net8.0/WellNexus.AssemblyInfo.cs
+++ WellNexus/obj/Debug/net8.0/WellNexus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WellNexus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fbd5d2e4291fb7eac753fcc4d539cf727bc2822")]
 [assembly: System.Reflection.AssemblyProductAttribute("WellNexus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WellNexus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
